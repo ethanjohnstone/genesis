e3c676f60e2077bb4e1c8af70a8351e2dd9bc27a
Ethan Johnstone <ethanjohnstone0911@gmail.com>
2020-01-25 11:30:48 +1300

    Added Hamburger variables for easy overwriting Added the seo titles things

--- app/src/pages/Page.php
+++ app/src/pages/Page.php
@@ -3,12 +3,28 @@
 namespace {
 
     use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Forms\TextField;
     use SilverStripe\Security\Permission;
 
     class Page extends SiteTree
     {
-        private static $db = [];
+        private static $db = [
+            "SeoTitle" => "Text"
+        ];
+
         private static $has_one = [];
+
+        public function getCMSFields()
+        {
+            $fields = parent::getCMSFields();
+
+            $fields->addFieldsToTab("Root.Main", [
+                TextField::create("SeoTitle")
+                    ->setDescription("For if you're wanting to some fancy flare to your page titles.")
+            ], "MenuTitle");
+
+            return $fields;
+        }
 
         public function IsAdmin()
         {
