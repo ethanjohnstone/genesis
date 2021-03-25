150b297bcd14443abfa3fe379f681470cbec6f41
NikxDa <NikxDa@users.noreply.github.com>
2021-03-25 23:05:12 +0100

    Update _config.php formatting

--- app/_config.php
+++ app/_config.php
@@ -1,12 +1,11 @@
 <?php
 
-// Namespaces
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 
 // Custom editor styles
-TinyMCEConfig::get('cms')
-    ->addButtonsToLine(1, 'styleselect')
+TinyMCEConfig::get("cms")
+    ->addButtonsToLine(1, "styleselect")
     ->setOptions([
-        'importcss_append' => true,
-        'style_formats' => []
+        "importcss_append" => true,
+        "style_formats" => []
     ]);
