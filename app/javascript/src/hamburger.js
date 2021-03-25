5ac5fd4448b79838ab2da17e65f4b5755490bb03
NikxDa <NikxDa@users.noreply.github.com>
2021-03-25 23:06:33 +0100

    Update SCSS/JS base

--- app/javascript/src/hamburger.js
+++
@@ -1,9 +0,0 @@
-let hamburger = document.querySelector(".header__hamburger");
-let navList = document.querySelector(".header__links");
-
-if (hamburger) {
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('is-active');
-        navList.classList.toggle('is-active');
-    })
-}
