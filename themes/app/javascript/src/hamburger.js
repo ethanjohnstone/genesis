e62764b3b15d65d174d6ce7d3967e0bfd38d78b9
Ethan Johnstone <ethanjohnstone0911@gmail.com>
2021-04-15 19:10:47 +1200

    Making sure we pass the linting rules

--- themes/app/javascript/src/hamburger.js
+++ themes/app/javascript/src/hamburger.js
@@ -2,8 +2,8 @@ let hamburger = document.querySelector(".header__hamburger");
 let navList = document.querySelector(".header__links");
 
 if (hamburger) {
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('is-active');
-        navList.classList.toggle('is-active');
-    })
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    navList.classList.toggle('is-active');
+  })
 }
