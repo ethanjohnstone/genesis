683fed182641a9fddfab78535f30ead5447be154
NikxDa <NikxDa@users.noreply.github.com>
2022-09-30 01:46:42 +0200

    Various changes
    - Cleanse dependencies
    - Add Tailwind
    - Add ESBuild
    - Remove Stylelint
    - Remove SCSS

--- app/javascript/main.tsx
+++ app/javascript/main.tsx
@@ -3,5 +3,5 @@ import { createRoot } from 'react-dom/client';
 import Main from "./components/Main";
 
 const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!);
 root.render(<Main />);
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,ss,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
